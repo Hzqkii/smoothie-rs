f1e51e3e81b1680687ed993dfd32d5747d020cb1
hybridkernel <admin@hzqki.me>
2025-01-19 21:46:35 +1100

    Default to using RIFE v3.1 for TTA as v4 isnt compatible

--- src/portable.rs
+++ src/portable.rs
@@ -1,14 +1,27 @@
-use std::{env, path::PathBuf, fs};
 use homedir;
+use std::{env, fs, path::PathBuf};
 
 const DEFAULT_RECIPE: &str = include_str!("../target/recipe.ini");
 const DEFAULT_ENCODING_PRESETS: &str = include_str!("../target/encoding_presets.ini");
 const DEFAULT_FLOWNET_BIN: &'static [u8] = include_bytes!("../target/models/rife-v4.6/flownet.bin");
 const DEFAULT_FLOWNET_PARAM: &str = include_str!("../target/models/rife-v4.6/flownet.param");
 
+const DEFAULT_TTA_CONTEXTNET_BIN: &'static [u8] =
+    include_bytes!("../target/models/rife-v3.1/contextnet.bin");
+const DEFAULT_TTA_CONTEXTNET_PARAM: &str =
+    include_str!("../target/models/rife-v3.1/contextnet.param");
+const DEFAULT_TTA_FLOWNET_BIN: &'static [u8] =
+    include_bytes!("../target/models/rife-v3.1/flownet.bin");
+const DEFAULT_TTA_FLOWNET_PARAM: &str = include_str!("../target/models/rife-v3.1/flownet.param");
+const DEFAULT_TTA_FUSIONNET_BIN: &'static [u8] =
+    include_bytes!("../target/models/rife-v3.1/fusionnet.bin");
+const DEFAULT_TTA_FUSIONNET_PARAM: &str =
+    include_str!("../target/models/rife-v3.1/fusionnet.param");
+
 fn get_target_path() -> PathBuf {
     let current_exe = env::current_exe().expect("Could not determine exe");
-    let target_dir = current_exe.parent()
+    let target_dir = current_exe
+        .parent()
         .expect("Could not get directory of executable")
         .parent()
         .expect("Could not get directory of directory's executable??");
@@ -22,7 +35,7 @@ fn is_portable() -> bool {
 
 pub fn get_config_path() -> PathBuf {
     let config_path: PathBuf;
-    
+
     if cfg!(target_os = "windows") || is_portable() {
         config_path = get_target_path();
     } else {
@@ -31,17 +44,16 @@ pub fn get_config_path() -> PathBuf {
             .expect("How do you not have a user dir?");
         config_path = home_dir.join(".config/smoothie-rs");
         if !config_path.exists() {
-            fs::create_dir_all(&config_path)
-                .expect("Failed to create config folder");
+            fs::create_dir_all(&config_path).expect("Failed to create config folder");
         }
-    } 
-    
+    }
+
     return config_path;
 }
 
 pub fn get_local_path() -> PathBuf {
     let local_path: PathBuf;
-    
+
     if cfg!(target_os = "windows") || is_portable() {
         local_path = get_target_path();
     } else {
@@ -50,8 +62,7 @@ pub fn get_local_path() -> PathBuf {
             .expect("How do you not have a user dir?");
         local_path = home_dir.join(".local/share/smoothie-rs");
         if !local_path.exists() {
-            fs::create_dir_all(&local_path)
-                .expect("Failed to create local folder");
+            fs::create_dir_all(&local_path).expect("Failed to create local folder");
         }
     }
 
@@ -85,9 +96,61 @@ pub fn get_default_model_path() -> PathBuf {
         fs::create_dir_all(&model_path).unwrap();
         fs::write(model_path.join("flownet.bin"), DEFAULT_FLOWNET_BIN).unwrap();
         fs::write(model_path.join("flownet.param"), DEFAULT_FLOWNET_PARAM).unwrap();
-    } else if !model_path.join("flownet.bin").exists() || !model_path.join("flownet.param").exists() {
+    } else if !model_path.join("flownet.bin").exists() || !model_path.join("flownet.param").exists()
+    {
         fs::write(model_path.join("flownet.bin"), DEFAULT_FLOWNET_BIN).unwrap();
         fs::write(model_path.join("flownet.param"), DEFAULT_FLOWNET_PARAM).unwrap();
+    }
+    return model_path;
+}
+
+pub fn get_default_model_tta_path() -> PathBuf {
+    let model_path = get_local_path().join("models/rife-v3.1");
+    if !model_path.exists() {
+        fs::create_dir_all(&model_path).unwrap();
+        fs::write(model_path.join("flownet.bin"), DEFAULT_TTA_FLOWNET_BIN).unwrap();
+        fs::write(model_path.join("flownet.param"), DEFAULT_TTA_FLOWNET_PARAM).unwrap();
+        fs::write(
+            model_path.join("contextnet.bin"),
+            DEFAULT_TTA_CONTEXTNET_BIN,
+        )
+        .unwrap();
+        fs::write(
+            model_path.join("contextnet.param"),
+            DEFAULT_TTA_CONTEXTNET_PARAM,
+        )
+        .unwrap();
+        fs::write(model_path.join("fusionnet.bin"), DEFAULT_TTA_FUSIONNET_BIN).unwrap();
+        fs::write(
+            model_path.join("fusionnet.param"),
+            DEFAULT_TTA_FUSIONNET_PARAM,
+        )
+        .unwrap();
+    } else if !model_path.join("flownet.bin").exists()
+        || !model_path.join("flownet.param").exists()
+        || !model_path.join("contextnet.bin").exists()
+        || !model_path.join("contextnet.param").exists()
+        || !model_path.join("fusionnet.bin").exists()
+        || !model_path.join("fusionnet.param").exists()
+    {
+        fs::write(model_path.join("flownet.bin"), DEFAULT_TTA_FLOWNET_BIN).unwrap();
+        fs::write(model_path.join("flownet.param"), DEFAULT_TTA_FLOWNET_PARAM).unwrap();
+        fs::write(
+            model_path.join("contextnet.bin"),
+            DEFAULT_TTA_CONTEXTNET_BIN,
+        )
+        .unwrap();
+        fs::write(
+            model_path.join("contextnet.param"),
+            DEFAULT_TTA_CONTEXTNET_PARAM,
+        )
+        .unwrap();
+        fs::write(model_path.join("fusionnet.bin"), DEFAULT_TTA_FUSIONNET_BIN).unwrap();
+        fs::write(
+            model_path.join("fusionnet.param"),
+            DEFAULT_TTA_FUSIONNET_PARAM,
+        )
+        .unwrap();
     }
     return model_path;
 }
@@ -96,20 +159,14 @@ pub fn get_last_args_path() -> PathBuf {
     let last_args: PathBuf;
 
     if cfg!(target_os = "windows") || is_portable() {
-        last_args = get_target_path()
-            .join("last_args.txt");
+        last_args = get_target_path().join("last_args.txt");
     } else {
         let home_dir = homedir::my_home()
             .unwrap()
             .expect("How do you not have a user dir?");
         last_args = home_dir.join(".local/share/smoothie-rs/last_args.txt");
         if !last_args.exists() {
-            fs::create_dir_all(
-                last_args
-                .parent()
-                .unwrap())
-                .expect("Failed to create local folder"
-                );
+            fs::create_dir_all(last_args.parent().unwrap()).expect("Failed to create local folder");
         }
     }
 
